9868f3e06dd8d12a1efce4be8a7c548dfb03baf2
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-15 21:10:24 +0400

    Add script for build all project (just beginning)

---
+++ GeneralBuild.fsx
@@ -0,0 +1,14 @@
+﻿#r "Fake/FakeLib.dll"
+#load "Packaging.fsx"
+
+open Fake 
+
+
+let pathToFakeExe = "../tools/Build.Tools/Fake/FAKE.exe"
+
+Target "Default" <| DoNothing
+
+"BuildWithoutTests"
+    ==> "Default"
+
+RunParameterTargetOrDefault "target" "Default"
