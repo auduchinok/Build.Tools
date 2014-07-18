3b1f41b8c8abd7c20df1548a44a85e90766e3a91
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-18 20:10:39 +0400

    Add function for changing standard map

--- Core.fsx
+++ Core.fsx
@@ -7,10 +7,12 @@
 #load "./Specflow.fsx"
 
 open System.IO
+open System.Collections.Generic
 open Fake
 
 let config = 
-    Map.ofList [
+    let dict = new Dictionary<_,_>()
+    [
         "build:configuration",          environVarOrDefault "configuration"         "Release"
         "build:solution",               environVar          "solution"
         "core:tools",                   environVar          "tools"
@@ -27,23 +29,30 @@ let config =
         "packaging:deploypushurl",      environVarOrDefault "deploypushurl"         ""
         "packaging:deployapikey",       environVarOrDefault "deployapikey"          ""
         "packaging:packages",           environVarOrDefault "packages"              ""
+        "test:path",                    environVarOrDefault "tests"                 ".\..\src\*\bin\Release\**\*.Tests.dll"
         "versioning:build",             environVarOrDefault "build_number"          "0"
         "versioning:branch",            match environVar "teamcity_build_branch" with
                                             | "<default>" -> environVar "vcsroot_branch"
                                             | _ -> environVar "teamcity_build_branch"
-        "vs:version",                   environVarOrDefault "vs_version"            "11.0" ]
+        "vs:version",                   environVarOrDefault "vs_version"            "11.0" 
+    ]
+    |> List.iter (fun (k,v) -> dict.Add(k,v))
+    dict
+
+let mapOfDict (dict : Dictionary<_,_>) =
+    dict |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Map.ofSeq  
 
 // Target definitions
 Target "Default"           <| DoNothing
-Target "Packaging:Package" <| Packaging.package config
-Target "Packaging:Restore" <| Packaging.restore config
-Target "Packaging:Update"  <| Packaging.update config
-Target "Packaging:Push"    <| Packaging.push config
-Target "Solution:Build"    <| Solution.build config
-Target "Solution:Clean"    <| Solution.clean config
-Target "Versioning:Update" <| Versioning.update config
-Target "Test:Run"          <| Test.run config
-Target "SpecFlow:Run"      <| Specflow.run config
+Target "Packaging:Package" <| Packaging.package (mapOfDict config)
+Target "Packaging:Restore" <| Packaging.restore (mapOfDict config)
+Target "Packaging:Update"  <| Packaging.update (mapOfDict config)
+Target "Packaging:Push"    <| Packaging.push (mapOfDict config)
+Target "Solution:Build"    <| Solution.build (mapOfDict config)
+Target "Solution:Clean"    <| Solution.clean (mapOfDict config)
+Target "Versioning:Update" <| Versioning.update (mapOfDict config)
+Target "Test:Run"          <| Test.run (mapOfDict config)
+Target "SpecFlow:Run"      <| Specflow.run (mapOfDict config)
 
 // Build order
 "Packaging:Restore"
@@ -56,4 +65,4 @@ Target "SpecFlow:Run"      <| Specflow.run config
     =?> ("Packaging:Push", not isLocalBuild)
     ==> "Default"
 
-RunParameterTargetOrDefault "target" "Default"
+//RunParameterTargetOrDefault "target" "Default"
