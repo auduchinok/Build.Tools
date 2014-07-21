9de156b9c23897a8d74451f5d3093f3c5a22a203
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-21 11:59:03 +0400

    Delete help code in Test script

--- Test.fsx
+++ Test.fsx
@@ -7,16 +7,7 @@ open System
 open System.IO
 
 let run (config : Map<string, string>) _ =
-    //let testDlls = !! (sprintf @"%s" (config.get "test:path"))
-    let pa = sprintf @"%s" (config.get "test:path")
-    printf "\n\nPATH  =  %s\n\n" pa
-    let pat = Path.GetFullPath(pa)
-    printf "PATH  =  %s\n\n" pat
-
     let testDlls = !! (sprintf @"%s" (config.get "test:path"))
-
-    printf "Count  =  %A\n\n" (Seq.length testDlls)
-
     if Seq.length testDlls > 0 then
         ensureNunitRunner config
         testDlls
