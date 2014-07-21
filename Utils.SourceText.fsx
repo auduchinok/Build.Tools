052d99662a86e2d358564bb77bd8eea0a13125d1
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-21 12:01:06 +0400

    Add Package target

--- Utils.SourceText.fsx
+++ Utils.SourceText.fsx
@@ -7,20 +7,24 @@ open Core
 let pathToSolution = @"..\src\YC.Utils.SourceText.sln"
 let pathToTests = @"..\Bin\Release\v40\Utils.SourceText.Tests.dll"
 let pathToTools = @"..\tools\Build.Tools"
+let pathToNuspec = @"..\src\YC.Utils.SourceText\Utils.SourceText.fsproj"
 
-let conf = config
-conf.["build:solution"] <- pathToSolution
-conf.["core:tools"] <- pathToTools
-conf.["test:path"] <- pathToTests
+config.["build:solution"] <- pathToSolution
+config.["core:tools"] <- pathToTools
+config.["test:path"] <- pathToTests
+config.["packaging:nuspecpath"] <- pathToNuspec
 
 Target "Clean"      <| Solution.clean (mapOfDict config)
 Target "Build"      <| Solution.build (mapOfDict config)
 Target "TestRun"    <| Test.run (mapOfDict config)
+Target "Package"    <| Packaging.package (mapOfDict config)
 Target "Def"        <| DoNothing
 
-"Clean"
-    ==> "Build"
-    ==> "TestRun"
+//"Clean"
+    //==> "Build"
+    //==> "TestRun"
+    //==> "Package"
+"Package"
     ==> "Def"
 
 RunParameterTargetOrDefault "target" "Def"
