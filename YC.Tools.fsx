8a3e172190c68d9c528af3585479b25829e81fdb
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-29 19:33:57 +0400

    Rewrite config using Core

--- YC.Tools.fsx
+++ YC.Tools.fsx
@@ -9,66 +9,26 @@ open System.IO
 let pathToRepository = @".."
 let pathToDll = @"..\Bin\Release\v40"
 let pathToSolution = @"..\src\YC.Tools.sln"
-//let pathToTests = @"..\Bin\Release\v40\*.Test.dll"
+let pathToTests = @"..\Bin\Release\v40\*.Test.dll"
 let pathToTools = @"..\tools\Build.Tools"
 let pathToPackages = @"..\src\packages"
-let pathToNuspec = @"..\src\YC.FsYacc\Tools.nuspec"
-let pathToNuspecFromRoot = @"src\YC.FsYacc\Tools.nuspec"
+let pathToNuspec = @"..\src\YC.FsYacc\YC.Tools.nuspec"
+let pathToNuspecFromRoot = @"src\YC.FsYacc\YC.Tools.nuspec"
 let pathToAssembleyInfo = @"..\src\YC.FsYacc\AssemblyInfo.fs"
 let pathToAssembleyInfoFromRoot = @"src\YC.FsYacc\AssemblyInfo.fs"
-let pushURL = @"https://www.myget.org/F/yc/api/v2/package"
-let pushApiKey = @"f6ba9139-9d42-4cf1-acaf-344f963ff807"
 
-let commitMessage = @"Change version of package in AssemblyInfo and Nuspec files (beta)"
-let gitCommandToCommit = sprintf "commit -m \"%s\" \"%s\" \"%s\"" commitMessage pathToAssembleyInfoFromRoot pathToNuspecFromRoot
-let gitUserName = "YcGeneralUser"
-let gitPassword = "yc2GeneralUser2014"
-let gitEmail = "yc.teamcity@gmail.com"
-let gitCommandToPush = sprintf "push --repo https://\"%s\":\"%s\"@github.com/YaccConstructor/YC.Utils.SourceText.git" gitUserName gitPassword
-let gitConfigUser = sprintf "config --global user.email \"%s\"" gitEmail
-let gitConfigEmail = sprintf "config --global user.name \"%s\"" gitUserName
+let specConfig = new SpecificConfig(pathToRepository, pathToDll, pathToSolution, pathToTests, pathToTools, pathToPackages, pathToNuspec, pathToNuspecFromRoot, pathToAssembleyInfo, pathToAssembleyInfoFromRoot)
+commonConfig specConfig
 
-config.["build:solution"] <- pathToSolution
-config.["core:tools"] <- pathToTools
-config.["bin:path"] <-pathToDll
-config.["repo:path"] <- pathToRepository
-//config.["test:path"] <- pathToTests
-config.["packaging:nuspecpath"] <- pathToNuspec
-config.["packaging:assemblyinfopath"] <- pathToAssembleyInfo
-config.["packaging:packages"] <- pathToPackages
-config.["packaging:deploypushurl"] <- pushURL
-config.["packaging:deployapikey"] <- pushApiKey
+"Packaging:Restore"
+    ==> "Solution:Clean"
+    ==> "Solution:Build"
+    //==> "Test:Run"
+    ==> "Versioning:Update"
+    ==> "Packaging:Package"
+    =?> ("Packaging:Push", not isLocalBuild)
+    =?> ("Git:Commit", not isLocalBuild)
+    =?> ("Git:Push", not isLocalBuild)
+    ==> "Default"
 
-Target "Version" (fun x ->
-    Versioning.update (mapOfDict config) x
-    Versioning.updateDeploy (mapOfDict config) x
-)
-Target "Commit" (fun _ ->
-    //gitCommand pathToRepository gitConfigUser
-    //gitCommand pathToRepository gitConfigEmail
-
-    gitCommand pathToRepository gitCommandToCommit
-)
-Target "PushChanges" (fun _ ->
-    gitCommand pathToRepository gitCommandToPush
-)
-Target "RestorePackages"    <| Packaging.restore (mapOfDict config)
-Target "Clean"              <| Solution.clean (mapOfDict config)
-Target "Build"              <| Solution.build (mapOfDict config)
-Target "TestRun"            <| Test.run (mapOfDict config)
-Target "Package"            <| Packaging.packageDeploy (mapOfDict config)
-Target "PushPackage"        <| Packaging.pushDeploy (mapOfDict config)
-Target "DefaultTarget"              <| DoNothing
-
-"RestorePackages"
-    ==>"Clean"
-    ==> "Build"
-    //==> "TestRun"
-    ==> "Version"
-    ==> "Package"
-    ==> "PushPackage"
-    //==> "Commit"
-    //==> "PushChanges"
-    ==> "DefaultTarget"
-
-RunParameterTargetOrDefault "target" "DefaultTarget"
+RunParameterTargetOrDefault "target" "Default"
