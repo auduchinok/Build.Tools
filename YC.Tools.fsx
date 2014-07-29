dbfd4fab68d11a1f1fbab20beaa781d0a72962c2
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-29 04:45:42 +0400

    Rewrite paths in YC.Tools build script

--- YC.Tools.fsx
+++ YC.Tools.fsx
@@ -8,14 +8,14 @@ open System.IO
 
 let pathToRepository = @".."
 let pathToDll = @"..\Bin\Release\v40"
-let pathToSolution = @"..\src\YC.Core.sln"
-let pathToTests = @"..\Bin\Release\v40\*.Test.dll"
+let pathToSolution = @"..\src\YC.Tools.sln"
+//let pathToTests = @"..\Bin\Release\v40\*.Test.dll"
 let pathToTools = @"..\tools\Build.Tools"
 let pathToPackages = @"..\src\packages"
-let pathToNuspec = @"..\src\YC.YaccConstructor\YC.Core.nuspec"
-let pathToNuspecFromRoot = @"src\YC.YaccConstructor\YC.Core.nuspec"
-let pathToAssembleyInfo = @"..\src\YC.YaccConstructor\AssemblyInfo.fs"
-let pathToAssembleyInfoFromRoot = @"src\YC.YaccConstructor\AssemblyInfo.fs"
+let pathToNuspec = @"..\src\YC.FsYacc\Tools.nuspec"
+let pathToNuspecFromRoot = @"src\YC.FsYacc\Tools.nuspec"
+let pathToAssembleyInfo = @"..\src\YC.FsYacc\AssemblyInfo.fs"
+let pathToAssembleyInfoFromRoot = @"src\YC.FsYacc\AssemblyInfo.fs"
 let pushURL = @"https://www.myget.org/F/yc/api/v2/package"
 let pushApiKey = @"f6ba9139-9d42-4cf1-acaf-344f963ff807"
 
@@ -32,7 +32,7 @@ config.["build:solution"] <- pathToSolution
 config.["core:tools"] <- pathToTools
 config.["bin:path"] <-pathToDll
 config.["repo:path"] <- pathToRepository
-config.["test:path"] <- pathToTests
+//config.["test:path"] <- pathToTests
 config.["packaging:nuspecpath"] <- pathToNuspec
 config.["packaging:assemblyinfopath"] <- pathToAssembleyInfo
 config.["packaging:packages"] <- pathToPackages
