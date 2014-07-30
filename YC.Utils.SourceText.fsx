fe7e347d877811f4724126bdae490d53b2212f73
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-30 12:12:51 +0400

    Correct paths in build script YC.Utils.SourceText

--- YC.Utils.SourceText.fsx
+++ YC.Utils.SourceText.fsx
@@ -12,10 +12,10 @@ let pathToSolution = @"..\src\YC.Utils.SourceText.sln"
 let pathToTests = @"..\Bin\Release\v40\*.Tests.dll"
 let pathToTools = @"..\tools\Build.Tools"
 let pathToPackages = @"..\src\packages"
-let pathToNuspec = @"..\src\YC.Utils.SourceText\Utils.SourceText.nuspec"
-let pathToNuspecFromRoot = @"src\YC.Utils.SourceText\Utils.SourceText.nuspec"
-let pathToAssembleyInfo = @"..\src\YC.Utils.SourceText\AssemblyInfo.fs"
-let pathToAssembleyInfoFromRoot = @"src\YC.Utils.SourceText\AssemblyInfo.fs"
+let pathToNuspec = @"..\src\Utils.SourceText\Utils.SourceText.nuspec"
+let pathToNuspecFromRoot = @"src\Utils.SourceText\Utils.SourceText.nuspec"
+let pathToAssembleyInfo = @"..\src\Utils.SourceText\AssemblyInfo.fs"
+let pathToAssembleyInfoFromRoot = @"src\Utils.SourceText\AssemblyInfo.fs"
 
 let specConfig = new SpecificConfig(pathToRepository, pathToDll, pathToSolution, pathToTests, pathToTools, pathToPackages, pathToNuspec, pathToNuspecFromRoot, pathToAssembleyInfo, pathToAssembleyInfoFromRoot)
 commonConfig specConfig
