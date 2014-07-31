d6a6bb624c99af9cf7ea2889a11f3ac044b1523a
2x2.4raymon <2x2.4raymon@gmail.com>
2014-07-31 21:25:50 +0400

    Add YC.SDK.SEL build script

---
+++ YC.SEL.SDK.fsx
@@ -0,0 +1,32 @@
+#r @"Fake/FakeLib.dll"
+#load "Core.fsx"
+
+open Fake
+open Fake.Git
+open Core
+open System.IO
+
+let pathToSolution = @"..\src\YC.Tools.sln"
+let pathToNuspec = @"..\src\FsYacc\YC.Tools.nuspec"
+let pathToNuspecFromRoot = @"src\FsYacc\YC.Tools.nuspec"
+let pathToAssembleyInfo = @"..\src\FsYacc\AssemblyInfo.fs"
+let pathToAssembleyInfoFromRoot = @"src\FsYacc\AssemblyInfo.fs"
+let gitUserName = "yc.TeamCity"
+let gitPassword = "my9UX2ka7XB3"
+let gitRepo = "code.google.com/p/recursive-ascent/"
+
+let specConfig = new SpecificConfig(pathToSolution, pathToNuspec, pathToNuspecFromRoot, pathToAssembleyInfo, pathToAssembleyInfoFromRoot, gitUserName, gitPassword, gitRepo)
+commonConfig specConfig
+
+"Packaging:Restore"
+    ==> "Solution:Clean"
+    ==> "Solution:Build"
+    //==> "Test:Run"
+    ==> "Versioning:Update"
+    ==> "Packaging:Package"
+    =?> ("Packaging:Push", not isLocalBuild)
+    //=?> ("Git:Commit", not isLocalBuild)
+    //=?> ("Git:Push", not isLocalBuild)
+    ==> "Default"
+
+RunParameterTargetOrDefault "target" "Default"
