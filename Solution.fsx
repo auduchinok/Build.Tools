aea93532eb3410235f9c412e3bd460c908d8f090
2x2.4raymon <2x2.4raymon@gmail.com>
2014-08-01 18:30:20 +0400

    Add functions to specify solution

--- Solution.fsx
+++ Solution.fsx
@@ -20,8 +20,28 @@ let private runCompiler target (config: Map<string, string>) =
     build setParams (config.get "build:solution")
 
 
+let private runCompilerSpec target (config: Map<string, string>) solution =
+    let setParams defaults =
+        { defaults with
+            Verbosity = Some(Quiet)
+            Targets = [target]
+            Properties =
+                [
+                    "Optimize", "True"
+                    "DebugSymbols", "True"
+                    "Configuration", config.get "build:configuration"
+                ]
+            MaxCpuCount = Some <| Some (1) }
+    build setParams solution
+
 let build (config: Map<string, string>) _ =
     runCompiler "Build" config
 
 let clean (config: Map<string, string>) _ =
     runCompiler "Clean" config
+
+let buildSpec (config: Map<string, string>) solution _ =
+    runCompilerSpec "Build" config solution
+
+let cleanSpec (config: Map<string, string>) solution _ =
+    runCompilerSpec "Clean" config solution
