d5e22d41b3a38931284ef200b2ff9ce5ae516172
Marina <polubelovam@gmail.com>
2015-02-17 00:49:17 +0400

    Change path to repository

--- YC.Core.fsx
+++ YC.Core.fsx
@@ -12,7 +12,7 @@ let pathToNuspec = @"..\src\YaccConstructor\YC.Core.nuspec"
 let pathToNuspecFromRoot = @"src\YaccConstructor\YC.Core.nuspec"
 let pathToAssembleyInfo = @"..\src\YaccConstructor\AssemblyInfo.fs"
 let pathToAssembleyInfoFromRoot = @"src\YaccConstructor\AssemblyInfo.fs"
-let gitRepo = "code.google.com/p/recursive-ascent/"
+let gitRepo = "github.com/YaccConstructor/YaccConstructor.git"
 
 let specConfig = new SpecificConfig(pathToSolution, pathToNuspec, pathToNuspecFromRoot, pathToAssembleyInfo, pathToAssembleyInfoFromRoot, gitRepo)
 commonConfig specConfig
